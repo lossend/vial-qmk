eb5cd5d6ba2b1e072fe97b6d3d9ea55ef7e7c7fd
yangjiesun <yangjie.sun@urbanic.com>
2022-09-03 13:05:05 +0800

    spring kle

---
+++ keyboards/owlab/spring/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x59, 0xBD, 0x2B, 0xB9, 0xC9, 0x89, 0x27, 0x4A}
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define VIAL_COMBO_ENTRIES 4
+#define VIAL_KEY_OVERRIDE_ENTRIES 4
---
+++ keyboards/owlab/spring/keymaps/vial/rules.mk
@@ -0,0 +1,7 @@
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+VIAL_ENABLE = yes
+NKRO_ENABLE = no
+VIAL_INSECURE = yes
+QMK_SETTINGS = no
+RGBLIGHT_ENABLE = yes   # Enable keyboard RGB underglow
